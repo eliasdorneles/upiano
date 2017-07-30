74f541bdc6ee808fc72e4db6d52f4efaaea36c31
Elias Dorneles <eliasdorneles@gmail.com>
2017-07-30 10:39:24 -0300

    let\'s make some noiiiseeee yoooooo

--- upiano.py
+++ upiano.py
@@ -1,32 +1,12 @@
-[ ] sketch an UI (https://en.wikipedia.org/wiki/Box-drawing_character)
-[ ] figure out how to play the notes
-[ ] hook UI events to note playing
+import urwid
+import subprocess
 
-
-
-┌ ─ ┐
-
-│   │
-
-│   │
-
-└ ─ ┘
-
-
-┌─┬─┬┬─┬─┬─┬─┬┬─┬┬─┬─┬─┬─┬┬─┬─┬─┬─┬┬─┬┬─┬─┐
-│ │█││█│ │ │█││█││█│ │ │█││█│ │ │█││█││█│ │
-│ │█││█│ │ │█││█││█│ │ │█││█│ │ │█││█││█│ │
-│ └┬┘└┬┘ │ └┬┘└┬┘└┬┘ │ └┬┘└┬┘ │ └┬┘└┬┘└┬┘ │
-│  │  │  │  │  │  │  │  │  │  │  │  │  │  │
-│  │  │  │  │  │  │  │  │  │  │  │  │  │  │
-└──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┘
-
-
-
+'''
+This is what we're gonna builddddd....
 ┌──┬───┬┬───┬──┬──┬───┬┬───┬┬───┬──┬──┬───┬┬───┬──┬──┬───┬┬───┬┬───┬──┐
 │  │███││███│  │  │███││███││███│  │  │███││███│  │  │███││███││███│  │
 │  │███││███│  │  │███││███││███│  │  │███││███│  │  │███││███││███│  │
-│  │█W█││█E█│  │  │█T█││█Y█││█U█│  │  │█I█││█O█│  │  │█]█││███││███│  │
+│  │█W█││█E█│  │  │█T█││█Y█││█U█│  │  │█O█││█P█│  │  │█]█││███││███│  │
 │  │███││███│  │  │███││███││███│  │  │███││███│  │  │███││███││███│  │
 
 │  └─┬─┘└─┬─┘  │  └─┬─┘└─┬─┘└─┬─┘  │  └─┬─┘└─┬─┘  │  └─┬─┘└─┬─┘└─┬─┘  │
@@ -35,7 +15,27 @@
 │    │    │    │    │    │    │    │    │    │    │    │    │    │    │
 │ C  │ D  │ E  │    │    │  A │ B  │ c  │ d  │ e  │    │    │    │    │
 └────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┴────┘
+'''
 
+NOTE_MAP = {
+    'A': 'C',
+    'W': 'C#',
+    'S': 'D',
+    'E': 'D#',
+    'D': 'E',
+    'F': 'F',
+    'T': 'F#',
+    'G': 'G',
+    'Y': 'G#',
+    'H': 'A',
+    'U': 'A#',
+    'J': 'B',
+    'K': 'C5',
+    'O': 'C#5',
+    'L': 'D5',
+    'P': 'D#5',
+    ';': 'E5',
+           }
 
 def play_note(note='C', duration=1, delay=0, vol=1, verbose=False):
     # requires sox to be installed: http://sox.sf.net
@@ -49,5 +49,14 @@ def play_note(note='C', duration=1, delay=0, vol=1, verbose=False):
 
     subprocess.Popen(command.split())
 
-    if delay:
-        wait(delay)
+def show_or_exit(key):
+    if key.upper() in NOTE_MAP:
+        txt.set_text(repr(key))
+        play_note(NOTE_MAP[key.upper()])
+    elif key.upper() == 'ESC':
+        raise urwid.ExitMainLoop()
+
+txt = urwid.Text(u"Play some piano, mannnnnnn!!!")
+fill = urwid.Filler(txt, 'top')
+loop = urwid.MainLoop(fill, unhandled_input=show_or_exit)
+loop.run()
